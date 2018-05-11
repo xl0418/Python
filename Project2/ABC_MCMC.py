81f9257a184a6b42b1a3d0b9399a368b43798f7b
Liang Xu <xl0418@gmail.com>
2018-05-11 15:23:27 +0200

    ABC_MCMC funs implemented

---
+++ Project2/ABC_MCMC.py
@@ -0,0 +1,50 @@
+import numpy as np
+from Trait_sim_in_branches_stat import traitsim, drawplot, dotplot
+
+
+def single_trait_sim(par):
+    sim = traitsim(h = 1, num_iteration=1,num_species=10,gamma1=par[0],gamma_K2=par[0],a = par[1],r = 1,theta = 0,K = 5000
+                   , mean_trait=0,dev_trait=20,mean_pop=50,dev_pop=20, num_time=2000)
+    return sim
+
+def calibrication(samplesize,priorpar,obs):
+    collection = np.zeros(shape=(samplesize,4))
+    uniform_gamma = np.random.uniform(priorpar[0],priorpar[1],samplesize)
+    uniform_a = np.random.uniform(priorpar[2],priorpar[3],samplesize)
+
+    for i in range(samplesize):
+        print(i)
+        par_cal = np.zeros(2)
+        par_cal[0] = uniform_gamma[i]
+        par_cal[1] = uniform_a[i]
+        sample_cal =  single_trait_sim(par_cal)
+        diff =  np.linalg.norm(sample_cal[0]-obs[0])
+        diff_sort = np.linalg.norm(np.sort(sample_cal[0])-np.sort(obs[0]))
+        collection[i] = np.concatenate((par_cal,[diff],[diff_sort]))
+    return collection
+
+
+
+
+def ABC_acceptance(par,delta,obs):
+    sample = single_trait_sim(par)
+    diff =  np.linalg.norm(sample[0]-obs[0])
+    if diff<delta:
+        return True
+    else:
+        return False
+
+def MCMC_ABC(startvalue, iterations,delta,obs):
+    MCMCchain = np.zeros(shape=(iterations+1,2))
+    MCMCchain[0,] = startvalue
+    par_jump = np.empty(2)
+    for i in range(iterations):
+        par_jump[0] = np.random.normal(loc=MCMCchain[i,0], scale= 1 )
+        par_jump[1] = np.random.normal(loc=MCMCchain[i,1], scale= 1 )
+        if (ABC_acceptance(par_jump,delta = delta, obs = obs)):
+            MCMCchain[i+1,] = par_jump
+        else:
+            MCMCchain[i + 1,] = MCMCchain[i ,]
+
+    return MCMCchain
+
---
+++ Project2/ABC_MCMC_run.py
@@ -0,0 +1,21 @@
+import numpy as np
+from Trait_sim_in_branches_stat import traitsim, drawplot, dotplot
+from ABC_MCMC import single_trait_sim,calibrication,ABC_acceptance,MCMC_ABC
+# Observation parameters
+par_obs = np.array([0.1,0.1])
+# Observation generated
+obs = traitsim(h = 1, num_iteration=1,num_species=10,gamma1=par_obs[0],gamma_K2=par_obs[0],a = par_obs[1],r = 1,\
+               theta = 0,K = 5000 , mean_trait=0,dev_trait=20,mean_pop=50,dev_pop=20, num_time=2000)
+# Threshold of acceptance
+delta = 0.01
+
+# Calibrication step
+cal_size = 100
+priorpar = [0.0001,1,0.0001,1]
+collection = calibrication(samplesize = cal_size, priorpar = priorpar, obs = obs)
+
+threshold = 0.05
+num = threshold*cal_size-1
+delta = np.sort(collection[:,3])[int(num)]
+
+
